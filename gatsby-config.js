72b73a7a7ccf28f0a83ddb7e43dbb224b8f59b34
Volodymyr Sauliak <3429638@gmail.com>
2020-10-16 19:36:21 +0300

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
         `gatsby-plugin-react-helmet`,
         `gatsby-source-data`,
         `gatsby-transformer-remark`,
+        `gatsby-plugin-favicon`,
         {
             resolve: `gatsby-source-filesystem`,
             options: {
@@ -30,6 +31,37 @@ module.exports = {
             options: {
                 sourceUrlPath: `fields.url`,
                 pageContextProperty: `menus`,
+            }
+        },
+          {
+            resolve: `gatsby-plugin-favicon`,
+            options: {
+                logo: "./src/favicon.png",
+
+                // WebApp Manifest Configuration
+                appName: null, // Inferred with your package.json
+                appDescription: null,
+                developerName: null,
+                developerURL: null,
+                dir: 'auto',
+                lang: 'Ua',
+                background: '#fff',
+                theme_color: '#fff',
+                display: 'standalone',
+                orientation: 'any',
+                start_url: '/?homescreen=1',
+                version: '1.0',
+
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: true,
+                    yandex: false,
+                    windows: false
+                }
             }
         }
     ]
